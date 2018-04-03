803392de92b4eb552250f7d1e7ddaf8072342668
stevenield <steve.nield@istein.ai>
2018-04-03 14:32:26 +0200

    Added appinsights support

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+if(process.env.APPINSIGHTS_INSTRUMENTATIONKEY){
+  require('applicationinsights').start();
+}
+
 var express = require('express');
 
 var app = express();
 
 app.set('port', process.env.PORT || 5557);
 
-app.get('/', function(req,res){
-    res.json({message: 'I am funky-gen'});
+app.get('/generate', function(req,res){
+  res.json({message: 'generate!'});
 });
 
 app.listen(app.get('port'), function () {
